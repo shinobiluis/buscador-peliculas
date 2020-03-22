341a60128ce506faa57f6f8cb6d8f0b0d53212a6
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-03-22 15:58:15 -0600

    Separamos el home como pagina

--- src/App.js
+++ src/App.js
@@ -2,48 +2,23 @@ import React, {Component} from 'react';
 import './App.css';
 // import { render } from '@testing-library/react';
 import 'bulma/css/bulma.css' //importamos bulma
-import {Title} from './components/title' // importamos el componente
-import { SearchForm } from './components/SearchForm'
 import {Detail} from './pages/Detail'
 
-import MoviesList from './components/MoviesList'
+import {Home} from './pages/Home'
 
 class App extends Component {
   // Creamos el state para renderizar resultados
-  state = {
-    usedSearch: false,
-    results: []
-  }
-  // Creamos el metodo que pasamos al componente hijo
-  _handleResults = (results) => {
-    this.setState({ results, usedSearch: true })
-  }
   
-  _renderResults(){
-      return this.state.results.length === 0
-        ? <p>No hay resultados 😵</p> 
-        : <MoviesList movies={this.state.results}/>
-  }
+  
   render(){
     const url = new URL(document.location)
-    const hasId = url.searchParams.has('id')
-    if(hasId){
-      return <Detail id={url.searchParams.get('id')}/>
-    }
+    const Page = url.searchParams.has('id')
+      ? <Detail id={url.searchParams.get('id')}/>
+      : <Home />
+    
     return (
       <div className="App">
-        <Title>Search movies</Title>
-        <div className="SearchForm-wrapper">
-          {/* Pasmos el onResults al componente hijo */}
-          <SearchForm onResults={this._handleResults}></SearchForm>
-        </div>
-        {/* Si el state results es === 0  */}
-        {
-          this.state.usedSearch
-          ? this._renderResults()
-          : <small>Use el formulario para buscar peliculas</small>
-        }
-        
+        {Page}
       </div>
     );
   }
