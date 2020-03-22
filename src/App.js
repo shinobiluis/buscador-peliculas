39c37a6d29b52c199efd0f7257fa29ac7099123e
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-03-22 12:38:53 -0600

    Creamos el fecth al api de imdb

--- src/App.js
+++ src/App.js
@@ -6,13 +6,35 @@ import {Title} from './components/title' // importamos el componente
 import { SearchForm } from './components/SearchForm'
 
 class App extends Component {
+  // Creamos el state para renderizar resultados
+  state = {
+    results: []
+  }
+  // Creamos el metodo que pasamos al componente hijo
+  _handleResults = (results) => {
+    this.setState({results})
+  }
+  // Metodo que retorna la actualizacion de resultados
+  _renderResults(){
+    const { results } = this.state
+    return results.map(movie => {
+      return <p key={movie.imdbID}>{movie.Title}</p>
+    })
+  }
   render(){
     return (
       <div className="App">
         <Title>Search movies</Title>
-        <div class="SearchForm-wrapper">
-          <SearchForm></SearchForm>
+        <div className="SearchForm-wrapper">
+          {/* Pasmos el onResults al componente hijo */}
+          <SearchForm onResults={this._handleResults}></SearchForm>
         </div>
+        {/* Si el state results es === 0  */}
+        {
+          this.state.results.length === 0
+          ? <p>Sin resultados.</p> 
+          : this._renderResults()
+        }
       </div>
     );
   }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,25 +1,43 @@
 import React, {Component} from 'react'
 
+// agregamos el api key de imdb
+const API_KEY = 'f7434544' 
+
 export class SearchForm extends Component{
+    // Creamos el state del formulario
     state = {
         inputMovie: ''
     }
+    // Controlamos el cambo del input actualizando el state
     _handleChange = (e) => {
         this.setState({
             inputMovie: e.target.value
         })
     }
+    // Controlamos el evneto submit del formulario
     _handleSubmit = (e) => {
+        // Creamos el fecht
         e.preventDefault()
-        alert(this.state.inputMovie)
+        const { inputMovie } = this.state
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+        .then(res => res.json())
+        .then(results => {
+            // console.log(results)
+            const { Search, totalResults } = results
+            console.log(Search)
+            // Le pasamos el search al onResults que proviene del padre
+            this.props.onResults(Search)
+        })
     }
     render(){
         return(
+            // Creamos el onSubmit
             <form onSubmit={this._handleSubmit}>
                 <div className="field has-addons">
                     <div className="control">
                         <input 
                             className="input" 
+                            // creamos el onChange
                             onChange={this._handleChange}
                             type="text" 
                             placeholder="Movies" />
